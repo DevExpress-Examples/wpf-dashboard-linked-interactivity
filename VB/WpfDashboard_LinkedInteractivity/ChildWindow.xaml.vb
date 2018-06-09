18db7ce540f6ab2e0fa5d6491ab78cbcb589ba47
DevExpressExampleBot <fakeEmail>
2018-06-09 15:36:22 +0300

    Source auto update [skip ci]

---
+++ VB/WpfDashboard_LinkedInteractivity/ChildWindow.xaml.vb
@@ -0,0 +1,85 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon.ViewerData
+Imports DevExpress.DashboardWpf
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+
+Namespace WpfDashboard_LinkedInteractivity
+    ''' <summary>
+    ''' Interaction logic for ChildWindow.xaml
+    ''' </summary>
+    Partial Public Class ChildWindow
+        Inherits Window
+
+        Private dControl As DashboardControl
+
+        Public Sub New(ByVal dControl As DashboardControl)
+            InitializeComponent()
+            Me.dControl = dControl
+        End Sub
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            childDashboardControl.LoadDashboard("Data\DashboardChild.xml")
+            Dim dParentControl As DashboardControl = Me.dControl
+            AddHandler dParentControl.MasterFilterSet, AddressOf DControl_MasterFilterSet
+            AddHandler dParentControl.MasterFilterCleared, AddressOf DControl_MasterFilterCleared
+            AddHandler dParentControl.DrillDownPerformed, AddressOf DControl_DrillDownPerformed
+            AddHandler dParentControl.DrillUpPerformed, AddressOf DControl_DrillUpPerformed
+        End Sub
+
+        Private Function HasDashboardItem(ByVal itemName As String) As Boolean
+            Return childDashboardControl.Dashboard.Items.Select(Function(i) i.ComponentName).Contains(itemName)
+        End Function
+
+        Private Sub DControl_MasterFilterSet(ByVal sender As Object, ByVal e As MasterFilterSetEventArgs)
+            If HasDashboardItem(e.DashboardItemName) Then
+                Dim itemName As String = e.DashboardItemName
+                If e.SelectedValues IsNot Nothing Then
+                    childDashboardControl.SetMasterFilter(itemName, e.SelectedValues)
+                End If
+                If e.SelectedRange IsNot Nothing Then
+                    childDashboardControl.SetRange(itemName, e.SelectedRange)
+                End If
+            End If
+        End Sub
+        Private Sub DControl_MasterFilterCleared(ByVal sender As Object, ByVal e As MasterFilterClearedEventArgs)
+            If HasDashboardItem(e.DashboardItemName) Then
+                Dim itemName As String = e.DashboardItemName
+                If childDashboardControl.CanClearMasterFilter(itemName) Then
+                    childDashboardControl.ClearMasterFilter(itemName)
+                End If
+            End If
+        End Sub
+        Private Sub DControl_DrillDownPerformed(ByVal sender As Object, ByVal e As DrillActionEventArgs)
+            If HasDashboardItem(e.DashboardItemName) Then
+                Dim itemName As String = e.DashboardItemName
+                Dim row As DashboardDataRow = e.Values(0)
+                Dim value As Object = row(row.Length - 1)
+
+                Dim tuple As IList(Of AxisPointTuple) = childDashboardControl.GetAvailableDrillDownValues(itemName)
+                Dim availableValues As IEnumerable(Of Object) = tuple.Select(Function(t) t.GetAxisPoint().UniqueValue)
+                If availableValues.Contains(value) Then
+                    childDashboardControl.PerformDrillDown(e.DashboardItemName, value)
+                End If
+            End If
+        End Sub
+        Private Sub DControl_DrillUpPerformed(ByVal sender As Object, ByVal e As DrillActionEventArgs)
+            If HasDashboardItem(e.DashboardItemName) Then
+                Dim itemName As String = e.DashboardItemName
+                Dim level As Integer = e.DrillDownLevel
+                Dim tuple As AxisPointTuple = childDashboardControl.GetCurrentDrillDownValues(itemName)
+                If tuple IsNot Nothing Then
+                    Dim point As AxisPoint = childDashboardControl.GetCurrentDrillDownValues(itemName).GetAxisPoint()
+                    Dim l As Integer = 0
+                    Do While point.Parent IsNot Nothing
+                        l += 1
+                        point = point.Parent
+                    Loop
+                    If (level + 1) = l AndAlso childDashboardControl.CanPerformDrillUp(itemName) Then
+                        childDashboardControl.PerformDrillUp(itemName)
+                    End If
+                End If
+            End If
+        End Sub
+    End Class
+End Namespace
---
+++ VB/WpfDashboard_LinkedInteractivity/MainWindow.xaml.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Windows
+
+Namespace WpfDashboard_LinkedInteractivity
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            mainDashboardControl.LoadDashboard("Data\Dashboard.xml")
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim childWindow1 As New ChildWindow(mainDashboardControl)
+            childWindow1.Show()
+        End Sub
+    End Class
+End Namespace
